9998439a62ff999bc81284536551b5a6bce9a05c
dadaiwei <dadaiwei@lvwan.com>
2019-08-27 20:48:29 +0800

    :art:Upload build files.

---
+++ build/precache-manifest.866f619ecfddb51509d07e0d4375fa90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a58a794eb5e40da236f4ea061cddca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e9c3bf092f6ebfddd11",
+    "url": "/static/js/2.cec8352f.chunk.js"
+  },
+  {
+    "revision": "0ab062d50f441d4488fc",
+    "url": "/static/js/main.19e84bd9.chunk.js"
+  },
+  {
+    "revision": "305d901e84d06feec9ef",
+    "url": "/static/js/runtime~main.a6a112a8.js"
+  }
+]);
